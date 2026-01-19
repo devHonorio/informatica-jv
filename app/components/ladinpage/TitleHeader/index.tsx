68cc8ba26d4516506cd584a05d354effd86b1d24
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2026-01-19 16:43:15 -0300

    fix: change size title header

--- app/components/ladinpage/TitleHeader/index.tsx
+++ app/components/ladinpage/TitleHeader/index.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import logo from "@/public/logo.png";
 export const TitleHeader = () => {
   return (
-    <div className="text-2xl font-bold tracking-tighter bg-linear-to-r from-cyan-400 to-purple-500 bg-clip-text text-transparent flex items-center">
+    <div className="md:text-2xl font-bold tracking-tighter bg-linear-to-r from-cyan-400 to-purple-500 bg-clip-text text-transparent flex items-center text-nowrap text-sm">
       <Image
         alt="Logo Informática JV"
         src={logo}
         width={100}
         height={100}
-        className="w-20 h-20"
+        className="md:w-20 md:h-20 w-12
+h-12 "
       />
       INFORMÁTICA JV
     </div>
