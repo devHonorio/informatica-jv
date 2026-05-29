cc972d950ed7c0139934a01657c5c14d7dc70070
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2026-05-29 16:03:30 -0300

    feat: update cv

--- app/components/ladinpage/DownLoadCurriculo/index.tsx
+++ app/components/ladinpage/DownLoadCurriculo/index.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function DownloadButton() {
   return (
     <Link
-      href="/JOSÉ HONORIO.pdf"
-      download="JOSÉ_HONORIO_CURRÍCULO.pdf"
+      href="/cv José Honorio Desenvoldor web node typescript.pdf"
+      download="cv José Honorio Desenvoldor web node typescript.pdf"
       className="flex items-center gap-2 bg-secondary hover:bg-secondary/80 border  px-6 py-3 rounded-2xl transition-all text-black"
     >
       <Download size={20} />
